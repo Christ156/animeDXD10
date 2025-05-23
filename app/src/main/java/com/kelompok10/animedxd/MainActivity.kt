7596f8eeba5ae3dcbd2f49e3384d4a2b78bb1e88
Christvaldo <christvaldo56@gmail.com>
2025-05-23 10:52:14 +0700

    test branch

--- app/src/main/java/com/kelompok10/animedxd/MainActivity.kt
+++ app/src/main/java/com/kelompok10/animedxd/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AnimeDXDTheme {
-        Greeting("Android")
+        Greeting("Christvaldo")
     }
 }
